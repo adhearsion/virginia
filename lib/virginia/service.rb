8f22f6236959ea34cf66a1644b1a82271b33ec4c
Luca Pradovera <luca.pradovera@gmail.com>
2013-02-02 16:02:53 +0100

    Now using a simplified handler for Reel

--- lib/virginia/service.rb
+++ lib/virginia/service.rb
@@ -3,15 +3,16 @@ require 'reel'
 module Virginia
   class Service
     def self.start
-      ::Reel::Server.supervise Adhearsion.config[:virginia].host, Adhearsion.config[:virginia].port do |connection|
-        while request = connection.request
-          case request
-          when Reel::Request
-            status, body = Adhearsion.config[:virginia].handler.handle request
-            connection.respond status, body
-          end
-        end
-      end
+      Adhearsion.config[:virginia].handler.new Adhearsion.config[:virginia].host, Adhearsion.config[:virginia].port
+      # ::Reel::Server.supervise Adhearsion.config[:virginia].host, Adhearsion.config[:virginia].port do |connection|
+      #   while request = connection.request
+      #     case request
+      #     when Reel::Request
+      #       status, body = Adhearsion.config[:virginia].handler.handle request
+      #       connection.respond status, body
+      #     end
+      #   end
+      # end
     end
   end
 end
