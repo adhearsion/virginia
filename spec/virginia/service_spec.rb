ebed4f22c97da87ebbc02cf5db8b5668d3eedce8
Ben Klang <bklang@mojolingo.com>
2014-10-31 12:18:16 -0400

    Update spec for new Rack launcher

--- spec/virginia/service_spec.rb
+++ spec/virginia/service_spec.rb
@@ -6,17 +6,24 @@ describe Virginia::Service do
     end
   end
 
-  let(:host) { "127.0.0.1" }
-  let(:port) { 8989 }
-  let(:handler) { DummyHandler }
+  let(:host)    { "127.0.0.1" }
+  let(:port)    { 8989 }
+  let(:options) { {Host: host, Port: port} }
+
   before :each do
-    Adhearsion.config.virginia.host = host
-    Adhearsion.config.virginia.port = port
-    Adhearsion.config.virginia.handler = handler
+    Adhearsion.stub(:root).and_return '.'
+    Adhearsion.config.virginia.host   = host
+    Adhearsion.config.virginia.port   = port
+    Adhearsion.config.virginia.rackup = 'spec/fixtures/config.ru'
   end
 
   it "should instantiate the handler" do
-    handler.should_receive(:new).with(host, port)
+    rack_logger = mock 'Rack::CommonLogger'
+    ::Rack::CommonLogger.should_receive(:new).once.with(TestApp, Adhearsion.logger).and_return rack_logger
+    ::Reel::Rack::Server.should_receive(:supervise_as).once.with(:reel_rack_server, rack_logger, options)
     Virginia::Service.start
   end
+end
+
+class TestApp
 end
